f61f18e57bebc55f232d420310cdd96fa976a0aa
pocke <p.ck.t22@gmail.com>
2014-03-31 21:04:44 +0900

    fix

--- lib/get_projects.rb
+++ lib/get_projects.rb
@@ -4,11 +4,15 @@ require_relative 'get_projects/matcher'
 module GetProjects
   module_function
   def get(project, opt = {})
-    address = nil
-    Matcher.any? do |key, matcher|
-      address = matcher.call(project, opt)
+    if opt[:getter] then
+      address = project
+    else
+      address = nil
+      Matcher.any? do |key, matcher|
+        address = matcher.call(project, opt)
+      end
+      raise Matcher::DontMatch unless address
     end
-    raise Matcher::DontMatch unless address
 
     Getter[opt[:getter]].call(address, opt)
   end
