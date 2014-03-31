38424537bbaa3b90fb5d35b975615a04d0b85a90
pocke <p.ck.t22@gmail.com>
2014-03-31 15:47:48 +0900

    create git getter

---
+++ lib/get_projects/getters/git.rb
@@ -0,0 +1,13 @@
+GetProjects::Getter::create(:git) do |url, opt|
+  if Dir::exist?(opt[:name]) then
+    Dir::chdir(opt[:name]) do
+      system('git', 'pull', '--rebase')
+    end
+  else
+    system('git', 'clone', url, opt[:name])
+  end
+
+  Dir::chdir(opt[:name]) do
+    system('git', 'submodule', 'update', '--init', '--recursive')
+  end
+end
---
+++ spec/lib/getters/git_spec.rb
@@ -0,0 +1,29 @@
+require 'fileutils'
+require 'tmpdir'
+require_relative '../../spec_helper'
+
+describe 'git getter' do
+  subject{GetProjects::Getter[:git]}
+
+  url = 'git@bitbucket.org:pocke/get_projects.git'
+  opt = {name: 'get_projects', getter: 'git'}
+
+  describe 'clone project' do
+    current_dir = Dir::pwd
+    tmp_dir = Dir::mktmpdir
+
+    before(:each) do
+      Dir::chdir(tmp_dir)
+      subject.call(url, opt)
+    end
+
+    after(:each) do
+      FileUtils::rm_rf(tmp_dir)
+      Dir::chdir(current_dir)
+    end
+
+    it do
+      expect(Dir::exist?(File::join(opt[:name], '.git'))).to be_true
+    end
+  end
+end
