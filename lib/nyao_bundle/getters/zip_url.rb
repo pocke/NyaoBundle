fafe3a54b5176eb0225b8e611ecdf4f0758f4542
pocke <p.ck.t22@gmail.com>
2014-04-11 09:45:16 +0900

    zip_url getter output

--- lib/nyao_bundle/getters/zip_url.rb
+++ lib/nyao_bundle/getters/zip_url.rb
@@ -8,9 +8,11 @@ NyaoBundle::Getter::create(:zip_url) do |url, opt|
     FileUtils::rm_rf(opt[:name], secure: true)
   end
 
+  puts "Downloading #{url} as #{opt[:name]}"
   Kernel::open(name, 'wb') do |file|
     Kernel::open(url) do |data|
       file.write(data.read)
     end
   end
+  puts 'Done!!'
 end
--- spec/lib/getters/zip_url_spec.rb
+++ spec/lib/getters/zip_url_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../spec_helper'
+require 'capture-output'
 
 describe 'zip url getter' do
   subject{NyaoBundle::Getter[:zip_url]}
@@ -13,5 +14,12 @@ describe 'zip url getter' do
 
   it do
     expect{subject.call(url, opt)}.not_to raise_error
+  end
+
+  it 'echo stdout' do
+    out = Capture.stdout do
+      subject.call(url, opt)
+    end
+    expect(out).to eq "Downloading #{url} as #{opt[:name]}\nDone!!\n"
   end
 end
